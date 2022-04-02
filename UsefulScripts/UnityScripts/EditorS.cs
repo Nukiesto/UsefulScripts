8b489eb909cdbe5b7c028a14ee6d257a92b67d6b
Nukiesto <58364599+Nukiesto@users.noreply.github.com>
2022-04-02 15:41:37 +0800

    Add EditorS

---
+++ UsefulScripts/UnityScripts/EditorS.cs
@@ -0,0 +1,37 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace UsefulScripts.UnityScripts
+{
+    public static class EditorS
+    {
+        public static void AddAssetToAsset(this Object origin, Object toAdd)
+        {
+#if UNITY_EDITOR
+            AssetDatabase.AddObjectToAsset(toAdd, origin);
+
+            origin.SaveChanges();
+#endif
+        }
+
+        public static void RemoveAssetFromAsset(this Object origin, Object toRemove)
+        {
+#if UNITY_EDITOR
+            AssetDatabase.RemoveObjectFromAsset(toRemove);
+            
+            EditorUtility.SetDirty(origin);
+            AssetDatabase.SaveAssets();
+#endif
+        }
+
+        public static void SaveChanges(this Object origin)
+        {
+#if UNITY_EDITOR     
+            EditorUtility.SetDirty(origin);
+            AssetDatabase.SaveAssetIfDirty(origin);
+#endif
+        }
+    }
+}
